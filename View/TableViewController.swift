7f3ce40bf8b590330f5228f01fb6ee154e7a02aa
ValikKabanchenko <kabanchenkoval@gmail.com>
2022-12-20 19:00:54 +0200

    New assets

--- View/TableViewController.swift
+++ View/TableViewController.swift
@@ -10,24 +10,6 @@ import UIKit
 class TableViewController: UITableViewController {
 
     
-    
-   
-    
-//    @IBAction func refreshControlAction(_ sender: Any) {
-//
-//        loadNews {
-//            DispatchQueue.main.async {
-//                self.refreshControl?.endRefreshing()
-//                self.tableView.reloadData()
-//
-//            }
-//
-//        }
-//
-//
-//    }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
